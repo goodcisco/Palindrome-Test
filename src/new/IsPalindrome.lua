4037d45bb38e395e737061a5381aefcad22889b6
goodcisco <126687757+goodcisco@users.noreply.github.com>
2023-03-14 18:39:39 +0100

    Update IsPalindrome.lua

--- src/new/IsPalindrome.lua
+++ src/new/IsPalindrome.lua
@@ -3,6 +3,8 @@
     thanks for his opinion on the previous version I realized there is an easier way to do it
 --]]
 
+-- Warnings: only works if the 1st and last letter is the same, the problem is that words that aren't palindrome like "dead" will count as palindrome
+
 local function isPalindrome(text)
     return string.sub(text, #text, #text) == string.sub(text, 1, 1)
 end
