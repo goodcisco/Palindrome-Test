6c04b0de9503e3539f708921c6df71bc4d943bda
goodcisco <126687757+goodcisco@users.noreply.github.com>
2023-03-25 19:17:30 +0100

    Update IsPalindrome.lua

--- src/new/IsPalindrome.lua
+++ src/new/IsPalindrome.lua
@@ -4,6 +4,7 @@
 --]]
 
 -- Warnings: only works if the 1st and last letter is the same, the problem is that words that aren't palindrome like "dead" will count as palindrome
+-- use the old version for precise results!
 
 local function isPalindrome(text)
     return string.sub(text, #text, #text) == string.sub(text, 1, 1)
